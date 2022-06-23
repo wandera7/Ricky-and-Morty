8c20fe1ba09205826acf38fff06a5e64db96866e
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-23 20:56:11 +0300

    display data

--- src/index.js
+++ src/index.js
@@ -15,3 +15,33 @@ function getCharacter(){
     .catch(err => console.error(err));
 
 }
+
+function handleDisplay(obj){
+    const main =document.querySelector('main')
+    main.innerHTML=`
+        <div class='card'>
+        <img src=${obj.image} alt="John" style="width:100%">
+        <h1 class="location">${obj.name}</h1>
+        <p class="title">${obj.gender}</p>
+        <p class="location">${obj.status}</p>
+        <p class="location" >${obj.location.name}</p>
+        <span class="location">Likes:<span id="likes" >0</span></sapn>
+        <p><button class='like location'>Like</button></p>
+        </div>
+        <section>
+        <form id="comment-form">   
+        <textarea id="review" class="review" ></textarea>
+        <button type="submit">Comment</button>
+        <h3>Comments:</h3>
+          </form>     
+           <ul id="comment-list">  
+           </ul>
+        </section>`
+       handleForm()
+          //Updating Likes
+         let count=0
+         document.querySelector('.like').addEventListener('click',()=>{
+         count++
+         document.querySelector("#likes").innerHTML=count
+          })
+}
